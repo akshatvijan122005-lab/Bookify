89bd17f60e58914bf65b2b83b4d236aa8139aa43
akshatvijan122005-lab <akshatvijan122005@gmail.com>
2026-02-13 16:26:27 +0530

    Update verifyAdminToken.js

--- backend/src/middleware/verifyAdminToken.js
+++ backend/src/middleware/verifyAdminToken.js
@@ -2,18 +2,24 @@ const jwt = require("jsonwebtoken");
 
 const jwtSecret = process.env.JWT_SECRET_KEY
 
-const verifyAdminToken = (req,res,next,err) =>{
+const verifyAdminToken = (req,res,next) =>{
     const token = req.headers['authorization']?.split(" ")[1];
     if(!token){
         return res.status(401).send({message : "Access Denied. No token provided"});
     }
-    jwt.verify(token,jwtSecret,(err,user)=>{
-        if(err){
-            return res.status(403).send({messaeg : "Invalid credentials"});
-        }
-        req.user = user;
-        next();
-    })
+    jwt.verify(token, jwtSecret, (err, user) => {
+    if (err) {
+        return res.status(403).send({ message: "Invalid token" });
+    }
+
+    if (user.role !== "admin") {
+        return res.status(403).send({ message: "Access denied. Admin only." });
+    }
+
+    req.user = user;
+    next();
+});
+
 }
 
 module.exports  = verifyAdminToken
