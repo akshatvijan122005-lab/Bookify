d1a7344a8e6271f95424a4d86604b94a6f06489e
akshatvijan122005-lab <akshatvijan122005@gmail.com>
2026-02-13 17:37:43 +0530

    Update userContoller.js

--- backend/src/users/userContoller.js
+++ backend/src/users/userContoller.js
@@ -1,27 +1,39 @@
 const jwt = require("jsonwebtoken");
 const User = require("./usermodel");
 
-module.exports.adminLogin = async(req,res)=>{
-    const {username , password} = req.body;
-    const jwtSecret = process.env.JWT_SECRET_KEY;
-    try{
-        const admin = await User.findOne({username})
-        if(!admin){
-            return res.status(404).send({message : "Admin not found!"})
-        }
-        if(password!==admin.password){
-           return  res.status(401).send({message : "Invalid password!"})
-        }
-        const token = jwt.sign({id: admin._id,username : admin.username,role : admin.role},
-            jwtSecret,
-            {expiresIn : "1h"}
-        )
-        return res.status(200).send({message : "Authentication successfull",token:token,user : {
-            username : admin.username,
-            role : admin.role
-        }})
-    }catch(err){
-        console.log("Failed to login the admin",err);
-        res.status(500).send({message : "Failed to login the admin"});
+const bcrypt = require("bcrypt");
+
+module.exports.adminLogin = async (req,res)=>{
+  const {username,password} = req.body;
+  const jwtSecret = process.env.JWT_SECRET_KEY;
+
+  try{
+    const admin = await User.findOne({username, role:"admin"});
+    if(!admin){
+      return res.status(404).send({message:"Admin not found"});
     }
+
+    const isMatch = await bcrypt.compare(password, admin.password);
+    if(!isMatch){
+      return res.status(401).send({message:"Invalid password"});
+    }
+
+    const token = jwt.sign(
+      {id:admin._id, username:admin.username, role:admin.role},
+      jwtSecret,
+      {expiresIn:"1h"}
+    );
+
+    res.status(200).send({
+      message:"Authentication successful",
+      token,
+      user:{
+        username:admin.username,
+        role:admin.role
+      }
+    });
+
+  }catch(err){
+    res.status(500).send({message:"Login failed"});
+  }
 }
